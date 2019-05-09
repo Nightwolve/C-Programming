64067001eb81dee9bdb25bff2a083b446ae399fe
Bart Grispen <bartstarss@gmail.com>
2019-05-09 18:55:32 +0200

    interrupts needed

--- Grispen_Claes_Matrix/Grispen_Claes_Matrix.ino
+++ Grispen_Claes_Matrix/Grispen_Claes_Matrix.ino
@@ -1,7 +1,7 @@
 /*
-* Auteur 1: Claes Ferre
-* Auteur 2: Grispen Bart
-* Link naar github repository: https://github.com/Nightwolve/C-Programming/tree/master/Grispen_Claes_Matrix
+  Auteur 1: Claes Ferre
+  Auteur 2: Grispen Bart
+  Link naar github repository: https://github.com/Nightwolve/C-Programming/tree/master/Grispen_Claes_Matrix
 */
 
 //Defining outputs
@@ -50,6 +50,11 @@ word smiley2  = 0xB615;
 word smiley3  = 0x4076;
 word smiley4  = 0x4E92;
 
+word smiley5  = 0b1101000100001010;
+word smiley6  = 0b1011011000010101;
+word smiley7  = 0b0100001001110010;
+word smiley8  = 0b0100110010010110;
+
 word leeg  = 0xD616;
 
 boolean shiftLeftRight = false;
@@ -65,93 +70,98 @@ void setup() {
 
 void loop()
 {
-   button_state = digitalRead(button); //reading from button switch
+  button_state = digitalRead(button); //reading from button switch
 
-  if (button_state ==LOW && button_state != last_button_state)//only reading button once per push
-   {
-       button_push_counter++;
-   }
-  else{}
-   last_button_state = button_state;
-   
-   switch (button_push_counter) //creating a switch so you get a character after every button press
-   {
+  if (button_state == LOW && button_state != last_button_state) //only reading button once per push
+  {
+    button_push_counter++;
+  }
+  else {}
+  last_button_state = button_state;
+
+  switch (button_push_counter) //creating a switch so you get a character after every button press
+  {
     case 0: //show the character B on the matrix
       letterB();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
-      
+
     case 1:
       letterA();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
-      
+
     case 2:
       letterR();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 3:
+    case 3:
       letterT();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 4:
+    case 4:
       ampersant();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 5:
+    case 5:
       letterF();
       Serial.print(button_push_counter);
       Serial.print('\n');
-      break; 
-      
-     case 6:
+      break;
+
+    case 6:
       letterE();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 7:
+    case 7:
       letterR();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 8:
+    case 8:
       letterR();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 9:
+    case 9:
       letterE();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 10:
+    case 10:
       smiley();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
 
-     case 11:
-      displayData(leeg);
+    case 11:
+      smileyFast();
       Serial.print(button_push_counter);
       Serial.print('\n');
       break;
-      
-     case 12:
+
+    case 12:
+      displayData(leeg);
+      Serial.print(button_push_counter);
+      Serial.print('\n');
+
+    case 13:
       button_push_counter = 0;
-      break;  
-   }
+      break;
+  }
 }
 
 void displayData(word message) //creating the shift register so your text will show correctly
@@ -171,64 +181,93 @@ void letterB() //functions that will show you the character B
   displayData(b1);
   displayData(b2);
   displayData(b3);
-  delay(1);
 }
 
 void letterA()
 {
- displayData(a1);
- displayData(a2);
- displayData(a3);
- delay(1);
+  displayData(a1);
+  displayData(a2);
+  displayData(a3);
+  delay(1);
 }
 
 void letterR()
 {
- displayData(r1);
- displayData(r2);
- displayData(r3);
- displayData(r4);
- delay(1);
+  displayData(r1);
+  displayData(r2);
+  displayData(r3);
+  displayData(r4);
+  delay(1);
 }
 
 void letterT()
 {
- displayData(t1);
- displayData(t2);
- delay(1);
+  displayData(t1);
+  displayData(t2);
+  delay(1);
 }
 
 void ampersant()
 {
- displayData(en1);
- displayData(en2);
- displayData(en3);
- displayData(en4);
- displayData(en5);
- delay(1);
+  displayData(en1);
+  displayData(en2);
+  displayData(en3);
+  displayData(en4);
+  displayData(en5);
+  delay(1);
 }
 
 void letterF()
 {
- displayData(f1);
- displayData(f2);
- displayData(f3);
- delay(1);
+  displayData(f1);
+  displayData(f2);
+  displayData(f3);
+  delay(1);
 }
 
 void letterE()
 {
- displayData(e1);
- displayData(e2);
- displayData(e3);
- delay(1);
+  displayData(e1);
+  displayData(e2);
+  displayData(e3);
+  delay(1);
 }
 
 void smiley()
 {
- displayData(smiley1);
- displayData(smiley2);
- displayData(smiley3);
- displayData(smiley4);
- delay(1); 
+  for (int i = 0; i < 1000; i++)
+  {
+    displayData(smiley1);
+    displayData(smiley2);
+    displayData(smiley3);
+    displayData(smiley4);
+    delay(1);
+  }
+
+  for (int i = 0; i < 1000; i++)
+  {
+    displayData(smiley5);
+    displayData(smiley6);
+    displayData(smiley7);
+    displayData(smiley8);
+  }
+}
+void smileyFast()
+{
+  for (int i = 0; i < 250; i++)
+  {
+    displayData(smiley1);
+    displayData(smiley2);
+    displayData(smiley3);
+    displayData(smiley4);
+    delay(1);
+  }
+
+  for (int i = 0; i < 250; i++)
+  {
+    displayData(smiley5);
+    displayData(smiley6);
+    displayData(smiley7);
+    displayData(smiley8);
+  }
 }
